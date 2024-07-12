51a33038dee017a5db15c5a1200f16002dc412e2
Om-Midya <dukemidya7521@gmail.com>
2024-07-13 00:29:30 +0530

    Boiler plate code for every page to work with expo routing

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const TabsLayout = () => {
+  return (
+    <View>
+      <Text>TabsLayout</Text>
+    </View>
+  );
+};
+
+export default TabsLayout;
