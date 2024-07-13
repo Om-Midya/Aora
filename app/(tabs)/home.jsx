5b1d2fce72cce7fe2d3283f8c355484bb02525e7
Om-Midya <dukemidya7521@gmail.com>
2024-07-14 01:26:19 +0530

    Working on Home Page UI

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Home</Text>
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="text-2xl text-semibold font-pmedium text-gray-100">
+                  Welcome back, John
+                </Text>
+                <Text className="text-lg text-secondary font-pmedium">
+                  Here are some of the latest updates
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-9"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+          </View>
+        )}
+      ></FlatList>
+    </SafeAreaView>
   );
 };
 
---
+++ components/SearchInput.jsx
@@ -0,0 +1,31 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+
+import { icons } from "../constants";
+
+const SearchInput = ({
+  title,
+  value,
+  placeholder,
+  handleChange,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className="w-full h-16 px-4 bg-black rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+      <TextInput
+        className="flex-1 text-white font-pregular mt-0.5 text-base"
+        value={value}
+        placeholder={placeholder}
+        onChangeText={handleChange}
+      />
+
+      <TouchableOpacity>
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
